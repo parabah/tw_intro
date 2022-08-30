3a63cac256916b76365c9d2f5ee0d8cc563eb4ed
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-08-30 14:48:35 -0300

    30/08/2022 - Primeira parte do layout mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,31 @@ module.exports = {
         'gray-ul-backg' : '#F1F1F1',
         'gray-li-head' : '#616161',
         'gray-li-bottom' : '#DDD'
+      },
+      borderRadius:{
+        'md' : '4px'
+      },
+      borderWidth: {
+        '5': '5px',
+      },
+      spacing: {
+        '5.5' : '22px',
+        '12.5': '60px',
+      },
+      lineHeight: {
+        '6.5' : '27px',
+        '8.5' : '33px',
+        '18' : '72px'
+      },
+      fontSize: {
+        'xl' : '22px',
+        '6xl' : '64px'
       }
     },
+    screens: {
+      'sm': '601px',
+      'lg': '993px'
+    }
   },
   plugins: [],
 }
