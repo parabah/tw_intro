a0bde95f15a591375f56060c6c49f32738ed691f
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-09-01 15:43:02 -0300

    01/09/2022 - Finalizada a implantação do layout exemplo

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,27 +11,35 @@ module.exports = {
         'gray-li-head' : '#616161',
         'gray-li-bottom' : '#DDD'
       },
-      spacing:{
-        '12.5': '60px'
-      },
       borderRadius:{
         'md' : '4px'
       },
       borderWidth: {
         '5': '5px',
       },
+      spacing: {
+        'tiny' : '0.16px',
+        '5.5' : '22px',
+        '12.5': '60px',
+        '17.5': '69px',
+        '21.5': '85px',
+        '23' : '91px'
+      },
       lineHeight: {
+        '6.5' : '27px',
         '8.5' : '33px',
+        '11' : '45px',
         '18' : '72px'
       },
       fontSize: {
+        'xl' : '22px',
         '6xl' : '64px'
       }
     },
     screens: {
       'sm': '601px',
       'lg': '993px'
-    },
+    }
   },
   plugins: [],
 }
